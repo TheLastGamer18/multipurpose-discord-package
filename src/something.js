85721bf2cf83c5aa6ff9deb7c49ce0d35b12538d
TheLastGamer18 <61044552+TheLastGamer18@users.noreply.github.com>
2021-04-19 15:02:13 +0530

    Update something.js

--- src/something.js
+++ src/something.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
 const mapping = {
   ' ': '   ',
   '0': ':zero:',
@@ -15,27 +15,27 @@ const mapping = {
   '?': ':grey_question:',
   '#': ':hash:',
   '*': ':asterisk:'
-};
+}
 
 class Something {
   
   async fetchAdvice() {
-    let json = await fetch("https://api.adviceslip.com/advice");
-    let advice = await json.json();
+    let json = await fetch("https://api.adviceslip.com/advice")
+    let advice = await json.json()
     
     if(!advice) {
-      throw new Error("[MDP] API Unavailable. Try again later.");
+      throw new Error("[MDP] API Unavailable. Try again later.")
     }
     
-    return advice.slip.advice;
+    return advice.slip.advice
   }
   
   async fetchJoke() {
-    let json = await fetch("https://jastinch-api.ml/randomjoke");
-    let joke = await json.json();
+    let json = await fetch("https://jastinch-api.ml/randomjoke")
+    let joke = await json.json()
     
     if(!joke) {
-      throw new Error("[MDP] Api Unavailable. Try again later.");
+      throw new Error("[MDP] Api Unavailable. Try again later.")
     }
     
     return {
@@ -49,110 +49,110 @@ class Something {
       throw new Error("[MDP] Provide a song name for it to work.")
     }
     
-    let json = await fetch(`https://jastinch-api.ml/lyrics?song=${song}`);
-    let lyrics = await json.json();
+    let json = await fetch(`https://jastinch-api.ml/lyrics?song=${song}`)
+    let lyrics = await json.json()
     
     if(!lyrics) {
       throw new Error("[MDP] No lyrics found.")
     }
     
-    return lyrics.lyrics;
+    return lyrics.lyrics
   }
 
   async fetchMeme() {
-    let subreddits = ["memes", "me_irl", "dankmemes", "comedyheaven", "Animemes"];
-    subreddits = subreddits[Math.floor(Math.random() * subreddits.length)];
+    let subreddits = ["memes", "me_irl", "dankmemes", "comedyheaven", "Animemes"]
+    subreddits = subreddits[Math.floor(Math.random() * subreddits.length)]
     
-    let json = await fetch(`https://reddit.com/r/${subreddits}/random/.json`);
-    let meme = await json.json();
-    
+    let json = await fetch(`https://reddit.com/r/${subreddits}/random/.json`)
+    let meme = await json.json()
+  
     if(!meme[0]) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    meme = meme[0].data.children[0].data;
+    meme = meme[0].data.children[0].data
     
     return {
       title: meme.title,
       image: meme.url
-    };
+    }
   }
   
   async fetchQuote() {
-    let json = await fetch("https://jastinch-api.ml/quotes");
-    let quotes = await json.json();
+    let json = await fetch("https://jastinch-api.ml/quotes")
+    let quotes = await json.json()
     
     if(!quotes) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return quotes.quote;
+    return quotes.quote
   }
   
   async fetchNeko() {
-    let json = await fetch("https://neko-love.xyz/api/v1/neko");
-    let neko = await json.json();
+    let json = await fetch("https://neko-love.xyz/api/v1/neko")
+    let neko = await json.json()
     
     if(!neko) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return neko.url;
+    return neko.url
   }
 
   async fetchNekoGif() {
-    let json = await fetch("https://nekos.life/api/v2/img/ngif");
-    let neko = await json.json();
+    let json = await fetch("https://nekos.life/api/v2/img/ngif")
+    let neko = await json.json()
     
     if(!neko) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return neko.url;
+    return neko.url
   }
   
   async fetchNSFWNeko() {
-    let json = await fetch("https://nekos.life/api/v2/img/lewd");
-    let neko = await json.json();
+    let json = await fetch("https://nekos.life/api/v2/img/lewd")
+    let neko = await json.json()
     
     if(!neko) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return neko.url;
+    return neko.url
   }
   
   async fetchNSFWNekoGif() {
-    let json = await fetch("https://nekos.life/api/v2/img/nsfw_neko_gif");
-    let neko = await json.json();
+    let json = await fetch("https://nekos.life/api/v2/img/nsfw_neko_gif")
+    let neko = await json.json()
     
     if(!neko) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return neko.url;
+    return neko.url
   }
   
   async fetchWaifu() {
-    let json = await fetch("https://waifu.pics/api/sfw/waifu");
-    let waifu = await json.json();
+    let json = await fetch("https://waifu.pics/api/sfw/waifu")
+    let waifu = await json.json()
     
     if(!waifu) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return waifu.url;
+    return waifu.url
   }
   
   async fetchNSFWWaifu() {
-    let json = await fetch("https://waifu.pics/api/nsfw/waifu");
-    let waifu = await json.json();
+    let json = await fetch("https://waifu.pics/api/nsfw/waifu")
+    let waifu = await json.json()
     
     if(!waifu) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return waifu.url;
+    return waifu.url
   }
   
   async owoify(text) {
@@ -160,14 +160,14 @@ class Something {
       throw new Error("[MDP] Provide some text to owoify!")
     }
     
-    let json = await fetch(`https://nekos.life/api/v2/owoify?text=${text}`);
-    let owo = await json.json();
+    let json = await fetch(`https://nekos.life/api/v2/owoify?text=${text}`)
+    let owo = await json.json()
     
     if(!owo) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return owo.owo;
+    return owo.owo
   }
   
   async emojify(text) {
@@ -177,9 +177,9 @@ class Something {
     
     "abcdefghijklmnopqrstuvwxyz".split("").forEach(c => {
       mapping[c] = mapping[c.toUpperCase()] = ` :regional_indicator_${c}:`
-    });
+    })
     
-    return text.split("").map(c => mapping[c] || c).join("");
+    return text.split("").map(c => mapping[c] || c).join("")
   }
   
   async reverse(text) {
@@ -187,7 +187,7 @@ class Something {
       throw new Error("[MDP] Provide some text to reverse!")
     }
     
-    return text.split("").reverse().join("");
+    return text.split("").reverse().join("")
   }
   
   async shuffle(text) {
@@ -199,69 +199,109 @@ class Something {
   }
   
   async fetchDogFact() {
-    let json = await fetch("https://some-random-api.ml/facts/dog");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/dog")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
   }  
   
   async fetchCatFact() {
-    let json = await fetch("https://some-random-api.ml/facts/cat");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/cat")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
   }
   
   async fetchBirdFact() {
-    let json = await fetch("https://some-random-api.ml/facts/bird");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/bird")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
   }
   
   async fetchKoalaFact() {
-    let json = await fetch("https://some-random-api.ml/facts/koala");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/koala")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
   }
   
   async fetchPandaFact() {
-    let json = await fetch("https://some-random-api.ml/facts/panda");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/panda")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
   }
   
   async fetchFoxFact() {
-    let json = await fetch("https://some-random-api.ml/facts/fox");
-    let fact = await json.json();
+    let json = await fetch("https://some-random-api.ml/facts/fox")
+    let fact = await json.json()
     
     if(!fact) {
       throw new Error("[MDP] API Unavailable. Try again later.")
-    };
+    }
     
-    return fact.fact;
+    return fact.fact
+  }
+  
+  async fetchRawMCSkin(name) {
+    let id = await fetch(`https://api.mojang.com/users/profiles/minecraft/${name}`)
+    if(id.status !== 200) {
+      throw new Error("[MDP] UUID isn't valid. Please check the name again.")
+    }
+    
+    let uuid = await id.json()
+    uuid = uuid.id
+    
+    let json = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
+    let profile = await json.json()
+    let skin = await JSON.parse(Buffer.from(profile.properties[0].value, 'base64').toString('utf-8'))
+      
+    return skin.textures.SKIN.url
+  }
+  
+  async fetchMCSkin(name) {
+    let id = await fetch(`https://api.mojang.com/users/profiles/minecraft/${name}`)
+    if(id.status !== 200) {
+      throw new Error("[MDP] UUID isn't valid. Please check the name again.")
+    }
+    
+    return `https://minotar.net/body/${name}/100.png`
+  }
+  
+  async fetchMCNameHistory(name) {
+    let id = await fetch(`https://api.mojang.com/users/profiles/minecraft/${name}`)
+    if(id.status !== 200) {
+      throw new Error("[MDP] UUID isn't valid. Please check the name again.")
+    }
+    
+    let uuid = await id.json()
+    uuid = uuid.id
+    
+    let json = await fetch(`https://api.mojang.com/user/profiles/${uuid}/names`)
+    
+    let names = await json.json()
+    return names.map(e => e.name)
   }
   
 }
