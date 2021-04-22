9f44ccdd39c731cc6ee79c84b859734690d00b66
TheLastGamer18 <61044552+TheLastGamer18@users.noreply.github.com>
2021-04-22 14:38:16 +0530

    Create function.js

---
+++ functions/function.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mapping: () => {
+    return {
+      ' ': '   ',
+      '0': ':zero:',
+      '1': ':one:',
+      '2': ':two:',
+      '3': ':three:',
+      '4': ':four:',
+      '5': ':five:',
+      '6': ':six:',
+      '7': ':seven:',
+      '8': ':eight:',
+      '9': ':nine:',
+      '!': ':grey_exclamation:',
+      '?': ':grey_question:',
+      '#': ':hash:',
+      '*': ':asterisk:'
+    }
+  }
+}
